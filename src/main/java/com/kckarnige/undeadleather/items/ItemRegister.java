0f49e9b6e24dbcf954bbfaf9e381afef695ac777
KCK <kayos156official@gmail.com>
2025-03-02 23:25:26 -0500

    1.0.2

--- src/main/java/com/kckarnige/undeadleather/items/ItemRegister.java
+++ src/main/java/com/kckarnige/undeadleather/items/ItemRegister.java
@@ -5,14 +5,19 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 
 public class ItemRegister {
-    public static Item ROTTEN_LEATHER = registerItem("rotten_leather", new Item(new Item.Settings()));
+    public static Item ROTTEN_LEATHER = registerItem("rotten_leather", new Item.Settings());
 
-    private static Item registerItem (String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(Undeadleather.MOD_ID, name), item);
+    private static Item registerItem (String id, Item.Settings item) {
+        // This is why plan ahead (don't ask why it took 2 commits to finish this thought)
+        // Idk why tf I'm talking, I didn't plan shit 💀
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Undeadleather.MOD_ID, id));
+        return Registry.register(Registries.ITEM, key, new Item(item.registryKey(key)));
     }
 
     public static void registerModItems () {
